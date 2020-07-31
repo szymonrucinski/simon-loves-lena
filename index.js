24d2653dff722a12b8bca0158fa68a67cd0273ee
Szymon Rucinski <s.rucinski@samsung.local>
2020-07-31 12:44:47 +0200

    changes

--- index.js
+++ index.js
@@ -21,37 +21,13 @@ app.use(bodyParser.urlencoded({
     type: 'application/json',
   }));
 app.use(upload());
-
+        
 
 app.get('/', (req, res) => {
 
    res.sendFile(__dirname + '/index.html' );
  
 });
-
-const uploadImage = async(req,res,next)=>{
-
-    var matches = req.body.base64Image.match(/^data:([A-Za-z+\/]+);base64,(.+)$/);
-    let response = {};
-    if(matches.length!==3)
-    {
-        return new Error('invalid input string')
-    }
-
-    response.type = matches[1];
-response.data = new Buffer(matches[2], 'base64');
-let decodedImg = response;
-let imageBuffer = decodedImg.data;
-let type = decodedImg.type;
-let extension = mime.extension(type);
-let fileName = "image." + extension;
-try {
-fs.writeFileSync("./images/" + fileName, imageBuffer, 'utf8');
-return res.send({"status":"success"});
-} catch (e) {
-next(e);
-}
-}
 
 app.post('/', (req,res)=>{
 
