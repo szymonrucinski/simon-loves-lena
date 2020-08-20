1dca27f12a9b92a84a74810b3d2e5d488ffb38ed
szymonrucinski <szymonrucinski@icloud.com>
2020-08-20 16:39:35 +0200

    linting added, fixed small issues

--- index.js
+++ index.js
@@ -1,59 +1,50 @@
-var processing = require('./build/Release/greet.node');
-const fs = require('fs');
-const mime = require('mime');
-const cp = require('child_process');
-const express = require('express');
-const path = require('path')
-const upload = require('express-fileupload')
-const bodyParser = require('body-parser');
-const e = require('express');
-
-const jsonParser = bodyParser.json()
+var processing = require("./build/Release/greet.node");
+const express = require("express");
+const upload = require("express-fileupload");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// app.use(express.static('upload'))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(express.json({
-    type: 'application/json',
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  express.json({
+    type: "application/json",
+  })
+);
 app.use(upload());
 
-
-app.get('/', (req, res) => {
-
-
-    res.send("Image Processing web API build by Szymon Rucinski")
+app.get("/", (req, res) => {
+  res.send("Image Processing web API build by Szymon Rucinski");
 });
 
-app.post('/brighter/:howBright', (req, res) => {
-
-    if (req.files) {
-        console.log(req.files);
-        console.log(req.body.value);
-        var file = req.files.file
-        var fileName = file.name
-
-        file.mv(__dirname + "/upload/" + fileName, (err) => {
-            if (err) {
-                res.send('ERROR WHILE UPLOADING')
-            }
-            else {
-                console.log(__dirname + "/upload/" + fileName);
-                processing.modBright(__dirname + "/upload/" + fileName, parseInt(req.body.value),fileName);
-                res.sendFile(__dirname + "/output/" + fileName);
-
-            }
-        })
-
-    }
+app.post("/brighter/:howBright", (req, res) => {
+  if (req.files) {
+    console.log(req.files);
+    console.log(req.body.value);
+    var file = req.files.file;
+    var fileName = file.name;
+    file.mv(__dirname + "/upload/" + fileName, (err) => {
+      if (err) {
+        res.send("ERROR WHILE UPLOADING");
+      } else {
+        console.log(__dirname + "/upload/" + fileName);
+        processing.modBright(
+          __dirname + "/upload/" + fileName,
+          parseInt(req.body.value),
+          fileName
+        );
+        res.sendFile(__dirname + "/output/" + fileName);
+      }
+    });
+  }
 });
-
 
 app.listen(PORT, () => {
-    console.log('Server running on port %d', PORT);
+  console.log("Server running on port %d", PORT);
 });
--- test.js
+++
@@ -1,3 +0,0 @@
-var processing = require('./build/Release/greet.node');
-const fileName = "bartSimpson.jpg"
-processing.modBright(__dirname + '/images/'+fileName, 200);
